765417e95fd6bd444ce50d6b81019b6452b74885
amana632 <amana632@gmail.com>
2018-07-22 10:44:02 +0530

    initial

---
+++ backend/main/model.py
@@ -0,0 +1,190 @@
+from main import db
+from flask_marshmallow import Marshmallow
+from sqlalchemy.dialects.mysql import BIGINT, BINARY, BIT, BLOB, BOOLEAN, CHAR, DATE, DATETIME, DECIMAL, DECIMAL, DOUBLE, ENUM, FLOAT, INTEGER, LONGBLOB, LONGTEXT, MEDIUMBLOB, MEDIUMINT, MEDIUMTEXT, NCHAR, NUMERIC, NVARCHAR, REAL, SET, SMALLINT, TEXT, TIME, TIMESTAMP,TINYBLOB, TINYINT, TINYTEXT, VARBINARY, VARCHAR, YEAR
+
+from main import ma
+
+
+class User(db.Model):
+    user_id = db.Column(db.String(255))
+    user_type = db.Column(db.String(255))
+    name = db.Column(db.String(255))
+    phone = db.Column(db.String(255), primary_key=True)
+    is_otp_verified = db.Column(db.Boolean)
+    address_id = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    is_email_verified = db.Column(db.Boolean)
+    password = db.Column(db.String(255))
+    area = db.Column(db.String(255))
+
+
+
+
+
+    def __init__(self, userid, user_type, name, phone, is_otp_verified, address_id, email, is_email_verified, password, area):
+        self.userid = userid
+        self.user_type = user_type
+        self.name = name
+        self.phone = phone
+        self.is_otp_verified = is_otp_verified
+        self.address_id = address_id
+        self.email = email
+        self.is_email_verified = is_email_verified
+        self.password = password
+        self.area = area
+
+	
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('userid', 'user_type', 'name', 'phone', 'is_otp_verified', 'address_id', 'email', 'is_email_verified', 'password', 'area')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+
+
+class Order(db.Model):                                                                
+    order_id = db.Column(db.String(255), primary_key=True)                                        
+    menu_data_dump = db.Column(db.NVARCHAR) 
+    chef_id = db.Column(db.String(255), db.ForeignKey('Schedule.chef_id'))                                    
+    merchant_id = db.Column(db.String(255), db.ForeignKey('Schedule.merchant_id')) 
+    amount = db.Column(db.Float(10))                       
+    total_amount = db.Column(db.Float(10))  
+    is_paid = db.Column(db.Boolean)
+
+                                        
+
+    def __init__(self, order_id, menu_data_dump, chef_id, merchant_id, amount, total_amount, is_paid):
+        self.order_id = order_id
+        self.menu_data_dump = menu_data_dump
+        self.chef_id = chef_id
+        self.merchant_id = merchant_id
+        self.amount = amount
+        self.total_amount = total_amount
+        self.is_paid = is_paid
+
+
+class OrderSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('order_id', 'menu_data_dump', 'chef_id', 'merchant_id', 'amount', 'total_amount', 'is_paid')
+
+
+order_schema = OrderSchema()
+orders_schema = OrderSchema(many=True)
+
+
+
+
+
+
+
+class Menu(db.Model):                                                                
+    menu_id = db.Column(db.String(255), primary_key=True)                                        
+    chef_id = db.Column(db.String(255), db.ForeignKey('Schedule.chef_id'))                                    
+    food_item_name = db.Column(db.String(255))                                      
+    price = db.Column(db.Float(10))                                   
+    day = db.Column(db.String(255))  
+    is_active = db.Column(db.Boolean)                                                                     
+    photo = db.Column(db.String(255))                                      
+
+
+    def __init__(self, menu_id, chef_id, food_item_name, price, day, is_active, photo):
+        self.menu_id = menu_id
+        self.chef_id = chef_id
+        self.food_item_name = food_item_name
+        self.price = price
+        self.day = day
+        self.is_active = is_active
+        self.photo = photo
+
+class MenuSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('menu_id', 'chef_id', 'food_item_name', 'price', 'day', 'is_active', 'photo')
+
+
+menu_schema = MenuSchema()
+menus_schema = MenuSchema(many=True)
+
+
+
+
+
+class Schedule(db.Model):         
+    schedule_id = db.Column(db.String(255), primary_key=True)                                                                                       
+    menu_data = db.Column(db.String(255))                                        
+    chef_id = db.Column(db.String(255))                                    
+    merchant_id = db.Column(db.String(255))                                      
+    amount = db.Column(db.Float(10))                       
+    total_amount = db.Column(db.Float(10))                       
+    date  = db.Column(db.DateTime)      
+
+    def __init__(self, menu_data, chef_id, merchant_id, amount, total_amount, date):
+        self.menu_data = menu_data
+        self.chef_id = chef_id
+        self.merchant_id = merchant_id
+        self.amount = amount
+        self.total_amount = total_amount
+        self.date = date
+
+      
+
+
+class ScheduleSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('menu_data', 'chef_id', 'merchant_id', 'amount', 'total_amount', 'date')
+
+
+schedule_schema = ScheduleSchema()
+schedules_schema = ScheduleSchema(many=True)
+
+
+
+
+
+
+
+class Address(db.Model):                                                                
+    address_id = db.Column(db.String(255), primary_key=True)                                        
+    user_type = db.Column(db.String(255))                                    
+    user_id = db.Column(db.String(255), db.ForeignKey('User.user_id'))
+    lat = db.Column(db.String(255))                                    
+    longitude = db.Column(db.String(255))    
+    address_line_one = db.Column(db.String(255))    
+    address_line_two = db.Column(db.String(255))    
+    pincode = db.Column(db.String(255))    
+    city = db.Column(db.String(255))    
+    landmark = db.Column(db.String(255))    
+
+                              
+
+      
+
+    def __init__(self, address_id, user_type, hotel_id, lat, longitude, address_line_one, address_line_two, pincode, city, landmark):
+        self.address_id = address_id
+        self.user_type = user_type
+        self.hotel_id = hotel_id
+        self.lat = lat
+        self.longitude = longitude
+        self.address_line_one = address_line_one
+        self.address_line_two = address_line_two
+        self.pincode = pincode
+        self.city = city
+        self.landmark = landmark
+
+
+class AddressSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('address_id', 'user_type', 'hotel_id', 'lat', 'longitude', 'address_line_one', 'address_line_two', 'pincode', 'city', 'landmark')
+
+
+address_schema = AddressSchema()
+addresss_schema = AddressSchema(many=True)
---
+++ backend/main/route.py
@@ -0,0 +1,7 @@
+from flask import request, jsonify
+from main import app
+from main.model import User
+from flask_marshmallow import Marshmallow
+from main import db
+
+
